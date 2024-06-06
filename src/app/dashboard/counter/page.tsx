5ebf11d58775195b184ec4a08905f2cb4b4a145b
EdiedRamos <edieisa22@gmail.com>
2024-06-05 20:20:03 -0500

    feat: configure Image component

---
+++ src/app/dashboard/counter/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Counter Page",
+  description: "Counter section",
+};
+
+const CounterPage = () => {
+  return <h1>Counter Page</h1>;
+};
+
+export default CounterPage;
---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,16 @@
+import { Sidebar } from "@/app/components";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function DashboardLayout({ children }: Props) {
+  return (
+    <div className="bg-slate-100 overflow-y-scroll w-screen h-screen antialiased text-slate-300 selection:bg-blue-600 selection:text-white">
+      <div className="flex">
+        <Sidebar />
+        <div className="text-black">{children}</div>
+      </div>
+    </div>
+  );
+}
