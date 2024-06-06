42c8d1ed4c39cdee47a2b546ad7ded2af2053cd0
EdiedRamos <edieisa22@gmail.com>
2024-06-06 02:05:30 -0500

    feat: render pokemons 😋

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,14 +1,39 @@
-const getPokemons = async (limit = 20, offset = 0) => {
-  const data = await fetch(
+import type { PokemonsResponse, SimplePokemon } from "@/app/pokemons";
+
+import Image from "next/image";
+
+const getPokemons = async (
+  limit = 20,
+  offset = 0
+): Promise<SimplePokemon[]> => {
+  const data: PokemonsResponse = await fetch(
     `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
   ).then((ans) => ans.json());
-  return data;
+  const pokemons: SimplePokemon[] = data.results.map(({ name, url }) => ({
+    id: url.split("/").at(-2) ?? "",
+    name,
+  }));
+  return pokemons;
 };
 
 const PokemonsPage = async () => {
   const pokemons = await getPokemons();
 
-  return <div>{JSON.stringify(pokemons)}</div>;
+  return (
+    <div className="flex flex-wrap justify-center gap-5 items-center p-3">
+      {pokemons.map((pokemon) => (
+        <div className="shadow-md w-[200px] flex flex-col gap-3 items-center rounded-xl bg-slate-800 p-5">
+          <Image
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
+            width={70}
+            height={70}
+            alt="Pokemon logo"
+          />
+          <p className="text-white font-medium">{pokemon.name}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default PokemonsPage;
