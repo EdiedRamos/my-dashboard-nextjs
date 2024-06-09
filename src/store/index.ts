88dda8c5880b202d7363e3766dee3b7fc11a0d27
EdiedRamos <edieisa22@gmail.com>
2024-06-09 18:32:08 -0500

    refactor: rename pokemons slice

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,7 +2,7 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./counter/counterSlice";
-import pokemonsReducer from "./pokemons/pokemons";
+import pokemonsReducer from "./pokemons/pokemonsSlice";
 
 export const store = configureStore({
   reducer: {
