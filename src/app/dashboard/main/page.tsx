6bc509a0780f181a3d50d81b19c81634f05dc52d
EdiedRamos <edieisa22@gmail.com>
2024-06-08 23:09:24 -0500

    feat: add main content

--- src/app/dashboard/main/page.tsx
+++ src/app/dashboard/main/page.tsx
@@ -1,5 +1,22 @@
+import { SimpleWidget } from "@/components";
+
 const MainPage = () => {
-  return <div>Main Page</div>;
+  return (
+    <div>
+      <h1 className="text-center text-2xl p-2 mb-3 bg-slate-800 text-white">
+        Dashboard
+      </h1>
+      <p>Información general</p>
+      <div className="flex flex-wrap gap-5 justify-center mt-5">
+        <SimpleWidget />
+        <SimpleWidget />
+        <SimpleWidget />
+        <SimpleWidget />
+        <SimpleWidget />
+        <SimpleWidget />
+      </div>
+    </div>
+  );
 };
 
 export default MainPage;
