154b1a7a041a74078565227d95ab860c0508173d
EdiedRamos <edieisa22@gmail.com>
2024-06-09 20:41:58 -0500

    feat: add favorite pokemons empty message

--- src/pokemons/components/FavoritePokemons.tsx
+++ src/pokemons/components/FavoritePokemons.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { PiSmileySadFill } from "react-icons/pi";
 import { PokemonGrid } from "./PokemonGrid";
 import { useAppSelector } from "@/store";
 
@@ -8,5 +9,30 @@ export const FavoriteGrid = () => {
     Object.values(store.pokemons.favoritePokemons)
   );
 
-  return <PokemonGrid pokemons={favorite} />;
+  return (
+    <>
+      {favorite.length === 0 ? (
+        <NotFound />
+      ) : (
+        <PokemonGrid pokemons={favorite} />
+      )}
+    </>
+  );
+};
+
+const NotFound = () => {
+  return (
+    <div className="bg-slate-100 antialiased text-black selection:bg-blue-600 selection:text-white">
+      <div className="flex">
+        <main className="relative w-full flex flex-col justify-center items-center bg-[#1A2238] p-10 rounded-lg">
+          <h1 className="text-9xl font-extrabold text-white tracking-widest">
+            <PiSmileySadFill className="text-yellow-500" />
+          </h1>
+          <div className="bg-[#FF6A3D] p-2 text-sm rounded text-white rotate-12 absolute top-5">
+            No tienes pokemones favoritos
+          </div>
+        </main>
+      </div>
+    </div>
+  );
 };
